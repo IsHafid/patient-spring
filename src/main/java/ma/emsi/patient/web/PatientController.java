22bc88fe8aee35df7191fed710baf34873c12928
IsmailHaf <53225171+IsmailHaf@users.noreply.github.com>
2022-04-07 22:15:04 +0000

    Pages + buttons to navigate

--- src/main/java/ma/emsi/patient/web/PatientController.java
+++ src/main/java/ma/emsi/patient/web/PatientController.java
@@ -3,9 +3,12 @@ package ma.emsi.patient.web;
 import lombok.AllArgsConstructor;
 import ma.emsi.patient.entities.Patient;
 import ma.emsi.patient.repo.PatientRepo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,12 @@ public class PatientController {
 
     private PatientRepo patientRepo;
     @GetMapping(path="/index")
-    public String patient(Model model){
-        List<Patient> patients = patientRepo.findAll();
-        model.addAttribute("listPatient",patients);
+    public String patient(Model model ,
+                          @RequestParam(name = "page",defaultValue="0")int page ,
+                          @RequestParam(name = "size",defaultValue="5")int size){
+        Page<Patient> Pagepatients = patientRepo.findAll(PageRequest.of(page,size));
+        model.addAttribute("listPatient",Pagepatients);
+        model.addAttribute("pages",new int[Pagepatients.getTotalPages()]);
         return "patients";
     }
 
