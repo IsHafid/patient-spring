cccab03c6ac3cb7b8b62c0fdce99a390052bfd65
IsmailHaf <53225171+IsmailHaf@users.noreply.github.com>
2022-04-20 14:06:32 +0000

    Delete button

--- src/main/java/ma/emsi/patient/web/PatientController.java
+++ src/main/java/ma/emsi/patient/web/PatientController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -27,7 +28,23 @@ public class PatientController {
         model.addAttribute("listPatient",Pagepatients);
         model.addAttribute("pages",new int[Pagepatients.getTotalPages()]);
         model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",keyword);
         return "patients";
+    }
+    @GetMapping("/delete")
+    public String delete(Long id, String keyword,int page){
+        patientRepo.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword"+keyword;
+    }
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> lispatient(){
+        return patientRepo.findAll();
     }
 
 }
